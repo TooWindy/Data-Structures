2c3739971943927e42e6041a17370630bb62ebc6
Chris E <14ceng@gmail.com>
2022-01-18 14:53:43 -0500

    More hard problems (Permutations + Anagrams)

--- Sliding-Window/Hard.js
+++ Sliding-Window/Hard.js
@@ -45,3 +45,94 @@ function length_of_longest_substring(str, k){
 }
 
 console.log(`Length of longest substring --- ${length_of_longest_substring('aabccbb', 2)}`)
+
+//Given a string and a pattern, find out if the string contains any permutation of the pattern.
+
+function find_permutation(str, pattern){
+  let windowStart = 0
+  let charFrequency = {}
+  let match = 0
+
+  //Fill hash map with the letters from pattern
+  for(let i=0;i<pattern.length;i++){
+    let char = pattern[i]
+    if(!(char in charFrequency)){
+      charFrequency[char] = 1
+    }
+    else{
+      charFrequency[char] ++
+    }
+  }
+
+  //Loop through string
+  for(let i=0;i<str.length;i++){
+    let rightChar = str[i]
+    if(rightChar in charFrequency){
+      charFrequency[rightChar] -= 1
+      if(charFrequency[rightChar] === 0){
+        match++
+      }
+    }
+    if(match === Object.keys(charFrequency).length){
+      return true
+    }
+    if(i - windowStart + 1 >= pattern.length){
+      let leftChar = str[windowStart]
+      if(leftChar in charFrequency){
+        if(charFrequency[leftChar] === 0){
+          match --
+        }
+        charFrequency[leftChar] += 1
+      }
+      windowStart++
+    }
+  }
+  return false
+}
+
+console.log(`Contains a permutation --- ${find_permutation('oidbcaf', 'abc')}`)
+
+//Given a string and a pattern, find all anagrams of the pattern in the given string
+
+function find_string_anagrams(str,pattern){
+  let windowStart = 0
+  let match = 0
+  let charFrequency = {}
+  let array = []
+
+  for(let i=0;i<pattern.length;i++){
+    let char = pattern[i]
+    if(!(char in charFrequency)){
+      charFrequency[char] = 0
+    }
+    charFrequency[char] ++
+  }
+
+  for(let i=0;i<str.length;i++){
+    let rightChar = str[i]
+    if(rightChar in charFrequency){
+      charFrequency[rightChar] -=1
+      if(charFrequency[rightChar] === 0){
+        match++
+      }
+    }
+
+    if(match === Object.keys(charFrequency).length){
+      array.push(i - pattern.length +1)
+    }
+
+    if(i - windowStart +1 >= pattern.length){
+      let leftChar = str[windowStart]
+      if(leftChar in charFrequency){
+        if(charFrequency[leftChar] === 0){
+          match --
+        }
+        charFrequency[leftChar] ++
+      }
+      windowStart++
+    }
+  }
+  return array
+}
+
+console.log(`Find anagrams by index --- ${find_string_anagrams('abbcabc', 'abc')}`)
